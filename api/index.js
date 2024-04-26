9b75a796dfd167db7b6353724ceac777942ddae9
Akhils777 <akhilpratapsingh777@gmail.com>
2024-04-26 15:18:07 +0530

    Complete sign up page and create sign in api route

--- api/index.js
+++ api/index.js
@@ -20,11 +20,11 @@ app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is runnung on port 3000');
-    }
-);
+    });
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
